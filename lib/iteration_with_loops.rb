2dbf5f2dcabc589059a483feb73321820df573d5
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-14 20:31:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,12 @@ def find_even_values(src)
     element_index = 0
     while element_index <
     src[row_index].count do
-      element_index += 1
+      if src[row_index]
+      [element_index].even? 
+            p src[row_index][element_index]
     end
+    element_index += 1
+  end
     row_index += 1
   end
 end
